5d9ff1f0a980bdc95722cfbb3e8768ce7424a680
Lucas Oliveira <lima1301lucas@gmail.com>
2025-02-19 17:21:41 -0300

    Ajuste no app e window

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
+from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QLabel
+from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   
-   window = QMainWindow()
+    #Cria a aplicação
+    app = QApplication(sys.argv)
+    window = MainWindow()
 
-   cw = QWidget()
-   v_layout = QVBoxLayout()
-   cw.setLayout(v_layout)
+    #Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
 
-   label1 = QLabel('O meu texto')
-   v_layout.addWidget(label1)
-   
-   window.setCentralWidget(cw)
-   window.show()
-
-   app.exec()
+    #Executa tudo
+    window.adjustFixedSize()
+    window.show()
+    app.exec()
