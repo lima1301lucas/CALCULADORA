af1caee2ed2d4ea1163532f2e8990b1b6c99ce06
Lucas Oliveira <lima1301lucas@gmail.com>
2025-02-19 10:41:27 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   
+   window = QMainWindow()
+
+   cw = QWidget()
+   v_layout = QVBoxLayout()
+   cw.setLayout(v_layout)
+
+   label1 = QLabel('O meu texto')
+   v_layout.addWidget(label1)
+   
+   window.setCentralWidget(cw)
+   window.show()
+
+   app.exec()
