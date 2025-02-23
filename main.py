fd1487d239e9ab2d4ca4c6e042537ff423be490b
Lucas Oliveira <lima1301lucas@gmail.com>
2025-02-22 22:39:35 -0300

    Alteração no Display

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 
 from main_window import MainWindow
+from display import Display
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
@@ -13,6 +14,10 @@ if __name__ == '__main__':
     #Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
+
+    #Display
+    display = Display()
+    window.addToVLayout(display)
 
     #Executa tudo
     window.adjustFixedSize()
