65a5287d21ef91d43c67f9810f5d386f3c32caec
Lucas Oliveira <lima1301lucas@gmail.com>
2025-02-25 22:14:04 -0300

    Ajustando bugs

--- buttons.py
+++ buttons.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QGridLayout, QPushButton
-from utils import isEmpty, isNumOrDot, isValidNumber
+from utils import isEmpty, isNumOrDot, isValidNumber, convertToNumber
 from variables import MEDIUM_FONT_SIZE
 
 if TYPE_CHECKING:
@@ -36,7 +36,7 @@ class ButtonsGrid(QGridLayout):
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
-            ['',  '0', '.', '='],
+            ['N',  '0', '.', '='],
         ]
         self.display = display
         self.window = window
@@ -58,15 +58,13 @@ class ButtonsGrid(QGridLayout):
     def equation(self, value):
         self._equation = value
         self.info.setText(value)
-    
-    def vouApagarVocê(self):
-        print('Signal recebido por "vouApagarVocê" em', type(self).__name__)
-
 
     def _makeGrid(self):
-        self.display.eqPressed.connect(self.vouApagarVocê)
-        self.display.delPressed.connect(self.display.backspace)
-        self.display.clearPressed.connect(self.vouApagarVocê)
+        self.display.eqPressed.connect(self._eq)
+        self.display.delPressed.connect(self._backspace)
+        self.display.clearPressed.connect(self._clear)
+        self.display.inputPressed.connect(self._insertToDisplay)
+        self.display.operatorPressed.connect(self._configLeftOp)
 
         for rowNumber, rowData in enumerate(self._gridMask):
             for colNumber, buttonText in enumerate(rowData):
@@ -77,7 +75,7 @@ class ButtonsGrid(QGridLayout):
                     self._configSpecialButton(button)
 
                 self.addWidget(button, rowNumber, colNumber)
-                slot = self._makeSlot(self._insertButtonTextToDisplay, button)
+                slot = self._makeSlot(self._insertToDisplay, buttonText)
                 self._connectButtonClicked(button, slot)
 
     def _connectButtonClicked(self, button, slot):
@@ -92,38 +90,56 @@ class ButtonsGrid(QGridLayout):
         if text == 'D':
             self._connectButtonClicked(button, self.display.backspace)
         
+        if text == 'N':
+            self._connectButtonClicked(button, self._invertNumber)
+        
         if text in '+-/*^':
-            self._connectButtonClicked(button, self._makeSlot(self._operatorClicked, button))
+            self._connectButtonClicked(button, self._makeSlot(self._configLeftOp, text))
         
         if text == '=':
             self._connectButtonClicked(button, self._eq)
 
+    @Slot()
     def _makeSlot(self, func, *args, **kwargs):
         @ Slot(bool)
         def realSlot(_):
             func(*args, **kwargs)
         return realSlot
 
-    def _insertButtonTextToDisplay(self, button):
-        buttonText = button.text()
-        newDisplayValue = self.display.text() + buttonText
+    @Slot()
+    def _invertNumber(self):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText):
+            return
+
+        number = convertToNumber(displayText) * -1
+        self.display.setText(str(number))
+
+    @Slot()
+    def _insertToDisplay(self, text):
+        newDisplayValue = self.display.text() + text
 
         if not isValidNumber(newDisplayValue):
             return
 
-        self.display.insert(buttonText)
+        self.display.insert(text)
+        self.display.setFocus() 
 
+    @Slot()
     def _clear(self):
         self._left = None
         self._right = None
         self._op = None
         self.equation = self._equationInitialValue
         self.display.clear()
+        self.display.setFocus() 
 
-    def _operatorClicked(self, button):
-        buttonText = button.text()  # +-/* (etc...)
+    @Slot()
+    def _configLeftOp(self, text):
         displayText = self.display.text()  # Deverá ser meu número _left
         self.display.clear()  # Limpa o display
+        self.display.setFocus() #Foco no display
 
         # Se a pessoa clicou no operador sem configurar qualquer número
         if not isValidNumber(displayText) and self._left is None:
@@ -132,25 +148,27 @@ class ButtonsGrid(QGridLayout):
 
         # Se houver algo no número da esquerda, não fazemos nada. Aguardaremos o número da direita.
         if self._left is None:
-            self._left = float(displayText)
+            self._left = convertToNumber(displayText)
 
-        self._op = buttonText
+        self._op = text
         self.equation = f'{self._left} {self._op}'
     
+    @Slot()
     def _eq(self):
         displayText = self.display.text()
 
-        if not isValidNumber(displayText):
+        if not isValidNumber(displayText) or self._left is None:
             self._showError('Conta incompleta')
             return
 
-        self._right = float(displayText)
+        self._right = convertToNumber(displayText)
         self.equation = f'{self._left} {self._op} {self._right}'
         result = 'error'
 
         try: 
-            if '^' in self._equation and isinstance(self._left, float):
+            if '^' in self._equation and isinstance(self._left, int | float):
                 result = math.pow(self._left, self._right)
+                result = convertToNumber(str(result))
             else:
                 result = eval(self.equation)
         except ZeroDivisionError:
@@ -162,10 +180,16 @@ class ButtonsGrid(QGridLayout):
         self.info.setText(f'{self._equation} = {result}')
         self._left = result
         self._right = None
+        self.display.setFocus() 
 
         if result == 'error':
             self._left = None
-    
+
+    @Slot()
+    def _backspace(self):
+        self.display.backspace()
+        self.display.setFocus() 
+        
     def _makeDialog(self, text):
         msgBox = self.window.makeMsgBox()
         msgBox.setText(text)
@@ -175,8 +199,10 @@ class ButtonsGrid(QGridLayout):
         msgBox = self._makeDialog(text)
         msgBox.setIcon(msgBox.Icon.Critical)
         msgBox.exec()
+        self.display.setFocus() 
 
     def _showInfo(self, text):
         msgBox = self._makeDialog(text)
         msgBox.setIcon(msgBox.Icon.Information)
         msgBox.exec()
+        self.display.setFocus() 
--- utils.py
+++ utils.py
@@ -16,3 +16,11 @@ def isValidNumber(string: str):
     except ValueError:
         valid = False
     return valid
+
+def convertToNumber(string: str):
+    number = float(string)
+    
+    if number.is_integer():
+        number = int(number)
+    
+    return number
